afcc5aed2abb8e39490afb3bb0e7a5d760081bd8
Ivanna Morales <ivannam@bu.edu>
2024-02-05 11:15:35 -0500

    admin page table added and functions for edit and delete buttons - not tested

--- client/src/Admin.js
+++ client/src/Admin.js
@@ -1,10 +1,35 @@
 import React from "react";
+import Table from "./table";
 
 function Admin(){
+    const adminPageColumns = ['Patient_ID', 'Exam_Id', 'Age', 'Sex', 'Zip',  'Image', 'Edit', 'Delete'];
+    //Function To Add edit or delete buttons to a cell
+    const makeButton = (colName, data, row, rowIndex) => {
+        if (colName == 'Delete'){
+            return <button onClick={()=> deleteExam(row, rowIndex)}>Delete</button>;
+        }
+        else if(colName=='Edit'){
+            return <button onClick={()=> editExam(row, rowIndex)}>Edit</button>;
+        }
+        else{
+            return data; 
+        }
+    };
+    // Fuction to delete data from a row
+    const deleteExam = (row, rowIndex) => {
+        console.log("Deleting Exam Data", rowIndex, row);
+    };
+    // Function to edit exam information
+    const editExam = (row, rowIndex) => {
+        console.log("Editing Exam Data", rowIndex, row);
+    };
+
     return(
+
         <div>
             <h1>Admin Page</h1>
-            {/* table content - tbd*/}
+            <Table data={response} cols={adminPageColumns} renderCell={makeButton}/>
         </div>
     )
 }
+export default Admin;
