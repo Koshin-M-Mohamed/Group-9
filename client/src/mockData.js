9b376e6348bca105bb216c7deacf279a3632b777
Koshin-M-Mohamed <koshin.m.mohamed@gmail.com>
2024-02-08 19:19:11 -0500

    fixed

--- client/src/mockData.js
+++ client/src/mockData.js
@@ -1,4 +1,4 @@
-export const fakeData =  Array.from({ length: 10 }, (_, i) => ({
+ const fakeData =  Array.from({ length: 10 }, (_, i) => ({
       Patient_ID: `PATIENT_${i + 1}`,
       Age: 20 + i + 1,
       Sex: (i + 1) % 2 === 0 ? "M" : "F",
@@ -11,3 +11,4 @@ export const fakeData =  Array.from({ length: 10 }, (_, i) => ({
       ICU_admits: (i + 1) % 3, // Cycles through 0, 1, 2
       Mortality: (i + 1) % 4 !== 0 ? "Alive" : "Deceased"
     }));
+    export default fakeData 
