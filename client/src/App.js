99d95730dd518765ee8b09ba0706abacaacdd773
Koshin-M-Mohamed <koshin.m.mohamed@gmail.com>
2024-02-10 17:43:41 -0500

    refined

--- client/src/App.js
+++ client/src/App.js
@@ -10,12 +10,12 @@ function App() {
   const columns = ['Patient_ID', 'Age', 'Sex', 'Zip', 'Latest_BMI', 'Latest_Weight', 'Image', 'Exam_Id', 'ICU_Admit', 'ICU_admits', 'Mortality'];
   return (
     <>
-      <div className='search-bar-Container'>
+      {/* <div className='search-bar-Container'>
         <div className='searchBar'>
           <input type="text" className="search-input" placeholder="Search..." />
           <button className="search-btn">Search</button>
         </div>
-      </div>
+      </div> */}
       <div className="App">
         <header className="App-header">
           {response && <Table data={response} cols={columns}/>}
