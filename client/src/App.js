c2fdf5e9c7cc32ab115529a85da965f7aaeee3d8
Ivanna Morales <ivannam@bu.edu>
2024-02-14 00:40:22 -0500

    linked to admin, search bar displayed above table

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import Table from './table';
+import Search from './Search.js';
 import fakeData from './mockData.js';
-import { useApi } from './hooks/use-api';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+//import { useApi } from './hooks/use-api';
+import { Link } from 'react-router-dom';
 
 function App() {
   //const { response } = useApi();
@@ -11,14 +12,10 @@ function App() {
   const columns = ['Patient_ID', 'Age', 'Sex', 'Zip', 'Latest_BMI', 'Latest_Weight', 'Image', 'Exam_Id', 'ICU_Admit', 'ICU_admits', 'Mortality'];
   return (
     <>
-      {/* <div className='search-bar-Container'>
-        <div className='searchBar'>
-          <input type="text" className="search-input" placeholder="Search..." />
-          <button className="search-btn">Search</button>
-        </div>
-      </div> */}
       <div className="App">
         <header className="App-header">
+          <Link to="/Admin">Admin Page</Link>
+          <Search />
           {response && <Table data={response} cols={columns}/>}
         </header>
       </div>
@@ -26,4 +23,5 @@ function App() {
   );
 }
 export default App;
+
 
