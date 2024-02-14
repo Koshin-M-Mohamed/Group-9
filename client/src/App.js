1e51abe17f712cddbefa281a025b649d5fdff489
Ivanna Morales <ivannam@bu.edu>
2024-02-13 23:45:41 -0500

    starting to link

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Table from './table';
 import fakeData from './mockData.js';
 import { useApi } from './hooks/use-api';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   //const { response } = useApi();
@@ -10,7 +11,7 @@ function App() {
   const columns = ['Patient_ID', 'Age', 'Sex', 'Zip', 'Latest_BMI', 'Latest_Weight', 'Image', 'Exam_Id', 'ICU_Admit', 'ICU_admits', 'Mortality'];
   return (  
     <div className="App">
-      <header className="App-header">
+      <header className="PatientRez">
           {response && <Table data={response} cols={columns}/>}
       </header>
     </div>
