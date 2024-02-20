aedad3b741cd47e7ab4db8f8270e84288a11494a
JuliansGHUB1 <105456402+JuliansGHUB1@users.noreply.github.com>
2024-02-20 17:29:54 -0500

    Made a schema and a model, and also made some endpoints and logic to handle requests to them

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const { getExamByPatientAndExamId, editExam } = require('../controllers/controller')
 
 /* GET home page. */
 
@@ -19,31 +20,51 @@ router.get('/patient', function(req,res,next){
   const PatientID = req.PATIENT_ID;
    
   // Parse the request and get the patientID and then pass it as an argument to the controller function
-    
+
   // Calls controller function to obtain a structure { {the first exam object pt}, {the second exam object pt}, ...}
 });
 
 // An endpoint to get particular information regarding an exam
-router.get('/exam', function(req,res,next){
+// Julian
+router.get('/exam/:PATIENT_ID/:exam_Id', async function(req, res, next) {
   // Calls controller function to obtain information regarding the particular exam
   // the req is going to be a set of 2 k-v pairs, the first being PatientId, the second being the ExamId
-
   // Parse the req object, and obtain both the patientID and the ExamId and store them in variables
+  const PATIENT_ID = req.params.PATIENT_ID;
+  const exam_Id = req.params.exam_Id;
 
+  try {
+    // Pass those variables as arguments to the controller function which will return an object containing information pertaining to exam
+    const exam = await getExamByPatientAndExamId(PATIENT_ID, exam_Id);
 
-
-  // Pass those variables as arguments to the controller function which will return an object containing information pertaining to exam
-
-
+    // Send the exam object as the response
+    res.json(exam);
+  } catch (error) {
+    // If an error occurs, pass it to the error handling middleware
+    next(error);
+  }
 });
 
 // An endpoint to update information regarding some exam
-router.put('/exam', function(req,res,next){
+// Julian 
+router.put('/exam/:PATIENT_ID/:exam_Id', async function(req,res,next){
   // The reuqest must be an object containing all the key value pairs, with the key value pairs updated (but not examID or patientID)
 
   // Parse the request for the patient ID/exam ID, and pass patientID, examID, and the updated object as arguments to the controller function
-
+  const PATIENT_ID = req.params.PATIENT_ID;
+  const exam_Id = req.params.exam_Id;
   // Controller function can return a copy of the updated entry
+  // console.log(req.body);
+  try {
+    // Pass those variables as arguments to the controller function which will return an object containing information pertaining to exam
+    const exam = await editExam(PATIENT_ID, exam_Id, req.body);
+
+    // Send the exam object as the response
+    res.json(exam);
+  } catch (error) {
+    
+    console.log(error);
+  }
 
 });
 
